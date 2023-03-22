489820c2f5a94733c6a1886c2761364b20047d46
wiederm <marcus.wieder@univie.ac.at>
2023-03-22 09:58:43 +0100

    example script

--- scripts/perform_correction.py
+++ scripts/perform_correction.py
@@ -1,111 +1,120 @@
 # general imports
+from openmm import LangevinIntegrator
 from openmm.app import (
     PME,
     CharmmParameterSet,
     CharmmPsfFile,
     CharmmCrdFile,
     PDBFile,
+    Simulation,
 )
 from endstate_correction.constant import zinc_systems, blacklist
 from endstate_correction.analysis import plot_endstate_correction_results
 import endstate_correction
 from endstate_correction.protocol import perform_endstate_correction, Protocol
 import mdtraj
-from openmm import unit
 import pickle, sys, os
-from endstate_correction.utils import convert_pickle_to_dcd_file
-########################################################
-########################################################
-# ------------ set up the waterbox system --------------
-# we use a system that is shipped with the repo
-package_path = endstate_correction.__path__[0]
-system_idx = int(sys.argv[1])
+from openmmml import MLPotential
+from openmmtools.utils import get_fastest_platform
 
-system_name = zinc_systems[system_idx][0]
-if system_name in blacklist:
-    print('System in blacklist. Aborting.')
-    sys.exit()
-env = "vacuum"
-print(system_name)
-print(env)
-# define the output directory
-parameter_base = f"{package_path}/data/hipen_data"
-# load the charmm specific files (psf, pdb, rtf, prm and str files)
-psf_file = f"{parameter_base}/{system_name}/{system_name}.psf"
-crd_file = f"{parameter_base}/{system_name}/{system_name}.crd"
+# ------------------- set up system -------------------#
+
+# load the charmm specific files (psf, rtf, prm and str files)
+psf_file = "test.psf"
+crd_file = "test.crd"
 psf = CharmmPsfFile(psf_file)
 crd = CharmmCrdFile(crd_file)
 params = CharmmParameterSet(
-    f"{parameter_base}/top_all36_cgenff.rtf",
-    f"{parameter_base}/par_all36_cgenff.prm",
-    f"{parameter_base}/{system_name}/{system_name}.str",
+    f"top_all36_cgenff.rtf",
+    f"par_all36_cgenff.prm",
+    f"parameter.str",
 )
+# write pdb file
+with open("temp.pdb", "w") as outfile:
+    PDBFile.writeFile(psf.topology, crd.positions, outfile)
 
 # define region that should be treated with the qml
 chains = list(psf.topology.chains())
 ml_atoms = [atom.index for atom in chains[0].atoms()]
-# define system
-
-sim = create_charmm_system(psf=psf, parameters=params, env=env, ml_atoms=ml_atoms)
-
-########################################################
-########################################################
-# ------------------- load samples ---------------------#
-n_samples = 5_000
-n_steps_per_sample = 1_000
-traj_base = output_base = f"/data/shared/projects/endstate_rew/{system_name}/"
-os.makedirs(f'{traj_base}/switching', exist_ok=True)
-mm_samples = []
-for i in range(1,4):
-    base = f"{traj_base}/sampling_charmmff/run0{i}/{system_name}_samples_{n_samples}_steps_{n_steps_per_sample}_lamb_0.0000" 
-    convert_pickle_to_dcd_file(f"{base}.pickle",psf_file, crd_file, f"{base}.dcd", "test.pdb" )
-    traj = mdtraj.load_dcd(
-            f"{base}.dcd",
-            top=psf_file,
-    )
-
-    mm_samples.extend(traj[1000:].xyz * unit.nanometer)  # NOTE: this is in nanometer!
-
-print(f'Initializing switch from {len(mm_samples)}')
-###############################
-qml_samples = []
-for i in range(1,4):
-    base = f"{traj_base}/sampling_charmmff/run0{i}/{system_name}_samples_{n_samples}_steps_{n_steps_per_sample}_lamb_1.0000"
-    convert_pickle_to_dcd_file(f"{base}.pickle",psf_file, crd_file, f"{base}.dcd", "test.pdb" )
-    traj = mdtraj.load_dcd(
-            f"{base}.dcd",
-            top=psf_file,
-    )
-
-    qml_samples.extend(traj[1000:].xyz * unit.nanometer)  # NOTE: this is in nanometer!
-print(f'Initializing switch from {len(mm_samples)}')
-####################################################
-# ----------------------- Correction ----------------------
-####################################################
-
-switching_length = 50_000
-print(f'{switching_length=}')
-neq_protocol = Protocol(
-    method="NEQ",
-    direction="bidirectional",
-    sim=sim,
-    trajectories=[mm_samples, qml_samples],
-    nr_of_switches=500,
-    neq_switching_length=switching_length,
+print(f"{ml_atoms=}")
+# generate MM system
+mm_system = psf.createSystem(params=params)
+# generate MM-QML mixed system
+potential = MLPotential("ani2x")
+ml_system = potential.createMixedSystem(
+    psf.topology, mm_system, ml_atoms, interpolate=True
 )
+#set up simulation object
+sim = Simulation(
+    psf.topology,
+    ml_system,
+    LangevinIntegrator(300, 1, 0.001),
+    platform=get_fastest_platform(minimum_precision="mixed"),
+)
+
+# ------------------- load samples --------------------#
+
+# define directory containing MM and QML sampling data
+# load MM samples
+trajs = []
+for i in range(1, 4): # load multiple trajectories
+    traj = mdtraj.load_dcd(
+        f"traj_{i}.dcd",
+        top=psf_file,
+    )
+    traj = traj[1000:]  # remove equilibration
+    trajs.append(traj)
+mm_samples = mdtraj.join(trajs) # join multiple trajectories
+print(f"Initializing switch from {len(mm_samples)} MM samples")
+
+# load QML samples
+qml_samples = []
+for i in range(1, 4):
+    traj = mdtraj.load_dcd(
+        f"traj_{i}.dcd",
+        top=psf_file,
+    )
+    traj = traj[1000:]  # remove equilibration
+    trajs.append(traj)
+qml_samples = mdtraj.join(trajs) # join multiple trajectories
+print(f"Initializing switch from {len(qml_samples)} QML samples")
+
+# ----------------- perform correction ----------------#
+
+# define the output directory
+output_base = f"switching/"
+os.makedirs(output_base, exist_ok=True)
+
+# ----------------------- FEP -------------------------#
 
 fep_protocol = Protocol(
     method="FEP",
-    direction="bidirectional",
     sim=sim,
-    trajectories=[mm_samples, qml_samples],
+    reference_samples=mm_samples,
+    target_samples=qml_samples,
     nr_of_switches=2_000,
 )
 
+# ----------------------- NEQ -------------------------#
 
+neq_protocol = Protocol(
+    method="NEQ",
+    sim=sim,
+    reference_samples=mm_samples,
+    target_samples=qml_samples,
+    nr_of_switches=500,
+    neq_switching_length=5_000,
+    save_endstates=True,
+    save_trajs=True,
+)
+
+# perform correction
 r_fep = perform_endstate_correction(fep_protocol)
-r_neq = perform_endstate_correction(neq_protocol, save_trajs=False)
-pickle.dump((r_neq, r_fep), open(f"{output_base}/switching/results_{switching_length}.pickle", 'wb'))
-plot_endstate_correction_results(system_name, r_neq, f"{output_base}/switching/results_bidirectional.png")
+r_neq = perform_endstate_correction(neq_protocol)
 
+# save fep and neq results in a pickle file
+pickle.dump((r_fep, r_neq), open(f"results.pickle", "wb"))
 
+# plot results
+plot_endstate_correction_results('mol', r_fep, f"{output_base}/results_neq.png")
+plot_endstate_correction_results('mol', r_neq, f"{output_base}/results_neq.png")
