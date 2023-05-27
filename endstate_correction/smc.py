78ca4a098e3b7c1254b1728f994cf91c4f052e95
wiederm <marcus.wieder@univie.ac.at>
2023-05-27 22:37:36 +0200

    tests still failing

--- endstate_correction/smc.py
+++ endstate_correction/smc.py
@@ -1,31 +1,27 @@
 # Perform SMC sampling for endstate correction
 
-import random
-from typing import Tuple
-
 import numpy as np
 from mdtraj import Trajectory
 from openmm import unit
 from openmm.app import Simulation
 from tqdm import tqdm
 
-from endstate_correction.constant import temperature
-from endstate_correction.system import get_positions
+from endstate_correction.constant import kBT
 
 
 def perform_SMC(
     sim: Simulation,
-    nr_of_steps: int = 1_000,
     samples: Trajectory,
-    nr_of_particles: int = 1_000,
+    nr_of_particles: int = 100,
+    nr_of_steps: int = 1_000,
 ) -> float:
     """Perform sequential MC to interpolate between reference and target potential.
 
     Args:
         sim (Simulation): simulation instance
-        samples (Trajectory): samples from which the starting points fo the NEQ switching simulation are drawn
+        samples (Trajectory): samples from which the starting points fo the SMC switching simulation are drawn
+        nr_of_particles (int, optional): number of particles. Defaults to 100.
         nr_of_steps (int, optional): number of interpolation steps. Defaults to 1000.
-        nr_of_particles (int, optional): number of particles. Defaults to 1000.
 
     Returns:
         np.array: weights of the particles
@@ -33,41 +29,59 @@ def perform_SMC(
 
     # list  of weights
     weights = np.ones(nr_of_particles) / nr_of_particles
-    t_values = np.linspace(0, 1, nr_of_steps)  # temperature values for the intermediate potentials
+    t_values = np.linspace(
+        0, 1, nr_of_steps
+    )  # temperature values for the intermediate potentials
 
+    # select initial samples
+    random_frame_idxs = np.random.choice(len(samples.xyz) - 1, size=nr_of_particles)
+
+    # select the coordinates of the random frame
+    particles = [
+        samples.openmm_positions(random_frame_idx)
+        for random_frame_idx in random_frame_idxs
+    ]
+
+    assert len(particles) == nr_of_particles
     # start with SMC
     for lamb in tqdm(t_values):
         # set lambda parameter
         sim.context.setParameter("lambda_interpolate", lamb)
         # calculate the current potentials for each particle
         u_intermediate = np.zeros(nr_of_particles)
-        
-        for p_idx, p in enumerate(samples):
+
+        for p_idx, p in enumerate(particles):
             sim.context.setPositions(p)
-            e_pot = sim.context.getState(getEnergy=True).getPotentialEnergy()            
-            u_intermediate[p_idx] = e_pot
-        
-        log_weights = np.log(weights) + u_intermediate - np.roll(u_intermediate, shift=1)
+            e_pot = sim.context.getState(getEnergy=True).getPotentialEnergy()
+            print(e_pot / kBT)
+            print(p_idx)
+            u_intermediate[p_idx] = e_pot / kBT
+
+        log_weights = (
+            np.log(weights) + u_intermediate - np.roll(u_intermediate, shift=1)
+        )
         log_weights -= np.max(log_weights)
         weights = np.exp(log_weights)
         weights /= np.sum(weights)
-        
-        # Resample the particles
-        indices = np.random.choice(nr_of_particles, size=nr_of_particles, p=weights)
-        particles = particles[indices]
 
+        # Resample the particles
+        particles = np.random.choice(particles, size=nr_of_particles, p=weights)
+        print(particles.shape)
+        print(particles)
         # Propagate the particles
-        _intermediate_samples = []
-        for p_idx, p in enumerate(samples):
+        _intermediate_particles = []
+        for p_idx, p in enumerate(particles):
             sim.context.setPositions(p)
             sim.step(1)
-            _intermediate_samples.append(sim.context.getState(getPositions=True).getPositions(asNumpy=True))
-        
-        samples = _intermediate_samples
+            _intermediate_particles.append(
+                sim.context.getState(getPositions=True).getPositions(asNumpy=True)
+            )
+
+        particles = _intermediate_particles
 
     # Calculate the free energy difference
-    free_energy_diff = -np.log(np.mean(np.exp(weights))))
+    free_energy_diff = -np.log(np.mean(np.exp(weights)))
 
     print(free_energy_diff)
- 
+
     return np.mean(u_intermediate)
---
+++ endstate_correction/tests/test_smc.py
@@ -0,0 +1,15 @@
+from .test_neq import load_endstate_system_and_samples
+from endstate_correction.smc import perform_SMC
+
+
+def test_SMC():
+    system_name = "ZINC00077329"
+    print(f"{system_name=}")
+
+    # load simulation and samples for ZINC00077329
+    sim, samples_mm, samples_qml = load_endstate_system_and_samples(
+        system_name=system_name,
+    )
+
+    # perform SMC switching
+    perform_SMC(sim=sim, nr_of_steps=1_000, samples=samples_mm, nr_of_particles=100)
