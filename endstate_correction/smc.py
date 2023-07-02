215e79ab26d388d8927de09ad282fdebce70425f
wiederm <marcus.wieder@univie.ac.at>
2023-07-02 20:16:55 +0200

    update SMC implementation

--- endstate_correction/smc.py
+++ endstate_correction/smc.py
@@ -4,6 +4,7 @@ import numpy as np
 from mdtraj import Trajectory
 from openmm import unit
 from openmm.app import Simulation
+from scipy.special import logsumexp
 from tqdm import tqdm
 
 from endstate_correction.constant import kBT, temperature
@@ -24,6 +25,7 @@ class SMC:
 
         self.sim = sim
         self.samples = samples
+        self.logZ = 0.0
 
     @staticmethod
     def _calculate_potential_E_for_particles(
@@ -58,7 +60,7 @@ class SMC:
         self,
         nr_of_walkers: int = 100,
         nr_of_steps: int = 1_000,
-    ):
+    ) -> None:
         """Perform SMC sampling
 
         Args:
@@ -67,36 +69,32 @@ class SMC:
         """
 
         # ------------------------- #
-        # Outline of the algorithm:
+        # Outline of the basic Sequential Importance Sampling (SIS) algorithm as outlined in
+        # 10.1021/acs.jctc.1c01198
         # ------------------------- #
-        # select random subset of walkers from the samples
+
+        # Goal: Calculate free energy estimate between reference distribution \pi(\lamb=0; x) and
+        # target distribution \pi(\lamb=1; x) with resampling steps
+
+        # Motivation: SMC might help focus the non equilibrium sampling on the region of interest;
+        # might help with RBFE calculations in analogy to the work of 10.1101/2020.07.29.227959 and avoid sampling of the
+        # target distribution \pi(\lamb=1; x) for bidirectional NEQ protocols
+
+        # obtain IID samples from the reference distribution \pi(\lamb=0; x)
         # initialize weights to be uniform
+        # select random subset of walkers from the samples
+        # for each interpolation step i between \lamb=0 and \lamb=1
+        #   propagate the walkers for a fixed number of steps with u(x; \lamb=i) to generate pool of IID samples
+        #   for each walker j
+        #       calculate the unnormalized weights w = exp(u(x; \lamb=i+1) - u(x; \lamb=i))
+        #   resample the walkers according to the normalized weights
 
-        # FOR each lambda in a sequence from 0 to 1:
-        #     calculate the intermediate potential for each particle U = (1 - lamb) * U_ref + lamb * (U_target - U_ref)
-        #     calculate the intermediate gradient for each particle
-        #     calculate the weights based on the ratio of successive intermediate potentials
-        #     update the weights based on the ratio of successive intermediate potentials
-        #     normalize the weights
-
-        # resample the particles based on the weights
-
-        # FOR each particle:
-        #     initialize velocity from a Maxwell-Boltzmann distribution corresponding to the temperature
-        #     update the state using Langevin dynamics, combining the gradient, the velocity, and Gaussian noise
-
-        # After all steps are completed, calculate the free energy difference:
-        #     compute the reference and target potentials for the final particles
-        #     compute the unnormalized weights as the exponential of the difference between the reference and target potentials
-        #     estimate the free energy difference as the negative logarithm of the average unnormalized weight
-        # ------------------------- #
+        # calculate the free energy estimate using the weights of each walker and the Zwanzig relation
 
         # initialize weights
         weights = np.ones(nr_of_walkers) / nr_of_walkers
         # initialize lambda values
-        lamb_values = np.linspace(0, 1, nr_of_steps)
-        # initialize potential energy matrix
-        pot_e = np.zeros((nr_of_walkers, nr_of_steps))
+        lambdas = np.linspace(0, 1, nr_of_steps)
 
         # select initial samples
         random_frame_idxs = np.random.choice(
@@ -109,37 +107,40 @@ class SMC:
 
         assert len(walkers) == nr_of_walkers
 
-        # calculate the initial potential for each particle
-        u_before = self._calculate_potential_E_for_particles(0.0, walkers, self.sim)
-
-        # for each lambda value calculate the intermediate potential for each particle
-        # and update the weights based on the ratio of successive intermediate potentials
-        for lamb in tqdm(lamb_values[1:]):  # exclude the first lambda value
-            u_now = self._calculate_potential_E_for_particles(lamb, walkers, self.sim)
-            # update the weights based on the ratio of successive intermediate potentials
-            log_weights = np.log(weights) + (u_now - u_before)
-            # NOTE: unsure if this is actually ture (shouldn't it be log(u_now/u_before)?))
-            # will leave this for now but revisit later!
-
-            log_weights -= np.max(log_weights)
-            weights = np.exp(log_weights)
-            weights /= np.sum(weights)
+        # start with switch
+        for lamb_idx in tqdm(range(len(lambdas) - 1)):
+            # set lambda parameter
+            self.sim.context.setParameter("lambda_interpolate", lambdas[lamb_idx])
+            # Propagate the walkers
+            walkers = self._propagate_particles(walkers, self.sim)
+            # calculate work
+            # evaluate U_(\lamb_(i+1))(x_i) -  U_(\lamb_(i))(x_i)
+            # calculate U_(\lamb_(i))(x_i)
+            u_now = self._calculate_potential_E_for_particles(
+                lambdas[lamb_idx], walkers, self.sim
+            )
+            # calculate U_(\lamb_(i+1))(x_i)
+            u_future = self._calculate_potential_E_for_particles(
+                lambdas[lamb_idx + 1], walkers, self.sim
+            )
+            # calculate weights (equation 2 in 10.1021/acs.jctc.1c01198)
+            current_deltaEs = u_future - u_now  # calculate difference in energy
+            current_weights = np.exp(
+                np.nanmin(current_deltaEs) - current_deltaEs
+            )  # subtract minimum to avoid numerical issues
+            current_weights /= np.sum(current_weights)  # normalize
+            # calculate ESS
+            ESS = 1.0 / np.sum(current_weights**2)
+            print(f"Effective Sample Size at lambda = {lambdas[lamb_idx]}: {ESS}")
+            print(current_weights)
+            # add to accumulated logZ
+            self.logZ += logsumexp(-current_deltaEs) - np.log(nr_of_walkers)
 
             # Resample the particles based on the weights
             random_frame_idxs = np.random.choice(
                 nr_of_walkers, size=nr_of_walkers, p=weights
             )
-            walkers = [
-                walkers[random_frame_idx] for random_frame_idx in random_frame_idxs
-            ]
+            walkers = [walkers[idx] for idx in random_frame_idxs]
 
-            # Propagate the particles
-            walkers = self._propagate_particles(walkers, self.sim)
-
-            # Calculate the free energy difference
-            free_energy_diff = -np.log(np.mean(np.exp(weights)))
-            # update potential energy vector
-            u_before = u_now
-            print(free_energy_diff)
-
-        return (free_energy_diff, pot_e)
+        # reset lambda value
+        self.sim.context.setParameter("lambda_interpolate", 0.0)
--- endstate_correction/tests/test_smc.py
+++ endstate_correction/tests/test_smc.py
@@ -1,6 +1,12 @@
-from .test_neq import load_endstate_system_and_samples
+import os
+
+import numpy as np
+import pytest
+from scipy.special import logsumexp
+
 from endstate_correction.smc import SMC
-import pytest, os
+
+from .test_system import setup_ZINC00077329_system
 
 
 @pytest.fixture
@@ -12,24 +18,62 @@ def _am_I_on_GH() -> bool:
         return True
 
 
+def test_SMC_algorithm():
+    # Define the potential function
+    def u(x, lam):
+        return -(x**2) + 100 * lam
+
+    # Function to evolve the configuration. In this toy example,
+    # let's assume it's a Gaussian random walk with standard deviation of 1.0
+    def evolve_configuration(x):
+        return x + np.random.normal(0, 1)
+
+    N_samples = 1000  # Number of samples
+    N_steps = 100  # Number of steps from lam=0 to lam=1
+
+    # Generate initial configurations
+    configurations = np.random.normal(0, 1, N_samples)
+    current_deltaEs = np.ones(N_samples)
+    logZ = 0.0
+    for i in range(N_steps):
+        lamb = i / N_steps
+        next_lamb = (i + 1) / N_steps
+
+        # importance sampling
+        for j in range(N_samples):
+            old_x = configurations[j]
+            new_x = evolve_configuration(old_x)
+            configurations[j] = new_x
+
+            # adjust weights based on energy difference
+            current_deltaEs[j] = u(new_x, next_lamb) - u(new_x, lamb)
+        current_weights = np.exp(
+            np.nanmin(current_deltaEs) - current_deltaEs
+        )  # subtract minimum to avoid numerical issues
+        current_weights /= np.sum(current_weights)  # normalize
+
+        # calculate ESS
+        ESS = 1.0 / np.sum(current_weights**2)
+        print(f"Effective Sample Size at lambda = {lamb}: {ESS}")
+
+        # resampling
+        indexes = np.random.choice(range(N_samples), size=N_samples, p=current_weights)
+        configurations = configurations[indexes]
+        weights = np.ones(N_samples) / N_samples
+
+        # calculate free energy difference
+        logZ += logsumexp(-current_deltaEs) - np.log(N_samples)
+
+    assert np.isclose(logZ, -100.0, atol=0.1)
+
+
 def test_SMC(_am_I_on_GH):
-    system_name = "ZINC00077329"
-    print(f"{system_name=}")
-
-    # load simulation and samples for ZINC00077329
-    sim, samples_mm, _ = load_endstate_system_and_samples(
-        system_name=system_name,
-    )
-
+    sim, samples_mm, samples_mm_qml = setup_ZINC00077329_system()
     smc_sampler = SMC(sim=sim, samples=samples_mm)
     # perform SMC switching
     print("Performing SMC switching")
     if _am_I_on_GH == True:
-        free_energy, pot_e = smc_sampler.perform_SMC(nr_of_steps=100, nr_of_walkers=10)
-        print(f"Am I on GH: {_am_I_on_GH}")
-
+        smc_sampler.perform_SMC(nr_of_steps=100, nr_of_walkers=2)
     else:
-        free_energy, pot_e = smc_sampler.perform_SMC(
-            nr_of_steps=1000, nr_of_walkers=100
-        )
-        print(f"Am I on GH: {_am_I_on_GH}")
+        smc_sampler.perform_SMC(nr_of_steps=100, nr_of_walkers=20)
+    print(smc_sampler.logZ)
