82fa329bc9954912749ef04ee2e67204c8faec04
wiederm <marcus.wieder@univie.ac.at>
2023-07-03 23:12:53 +0200

    update tests

--- endstate_correction/tests/test_smc.py
+++ endstate_correction/tests/test_smc.py
@@ -73,7 +73,7 @@ def test_SMC(_am_I_on_GH):
     # perform SMC switching
     print("Performing SMC switching")
     if _am_I_on_GH == True:
-        smc_sampler.perform_SMC(nr_of_steps=100, nr_of_walkers=2)
+        smc_sampler.perform_SMC(nr_of_steps=1, nr_of_walkers=2)
     else:
-        smc_sampler.perform_SMC(nr_of_steps=100, nr_of_walkers=20)
+        smc_sampler.perform_SMC(nr_of_steps=5, nr_of_walkers=100)
     print(smc_sampler.logZ)
